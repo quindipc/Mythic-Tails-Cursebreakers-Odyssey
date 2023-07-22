ce4cd0c044210c9da00c99b31e6b51c67c743a67
Chelsea Quindipan <quindipc@gmail.com>
2023-07-22 19:43:05 -0400

    add dark mode for game and grey color variable

--- client/src/components/GameWindow/GameWindow.jsx
+++ client/src/components/GameWindow/GameWindow.jsx
@@ -3,25 +3,33 @@ import React, { useState } from "react";
 import "./GameWindow.scss";
 
 const GameWindow = () => {
-// PROGRESS
-  const [progress, setProgess] = useState(0)
-  const [gameText, setGameText] = useState("Wecome to Mythic Tails: Cursebreaker's Odyssey")
+  // PROGRESS
+  const [progress, setProgess] = useState(0);
+  const [gameText, setGameText] = useState(
+    "Wecome to Mythic Tails: Cursebreaker's Odyssey",
+  );
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  // DARK MODE TOGGLE
   const handleDarkMode = () => {
-    const toggleContainer = document.querySelector(".game__toggle-container")
-    toggleContainer.classList.toggle("dark");
+    setIsDarkMode((prevMode) => !prevMode);
   };
 
   return (
-    <div className="game">
+    <div className={`game ${isDarkMode ? "game__dark" : ""}`}>
       <div className="game__toggle-container">
-        <input type="checkbox"
-          id="switch" onClick={handleDarkMode} />
-        <label className="game__switch" htmlFor="switch"></label>
-        <div className="game__toggle"></div>
+        <input
+          type="checkbox"
+          id="switch"
+          checked={isDarkMode}
+          onChange={handleDarkMode}
+        />
+        <label className="game__switch-label" htmlFor="switch">
+          <div className="game__toggle"></div>
+        </label>
       </div>
       <h1 className="game__title">{gameText}</h1>
-  </div>
-  )
-}
+    </div>
+  );
+};
 export default GameWindow;
