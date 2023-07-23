f93e28594788b577ca93237178e602c91d92fabe
Chelsea Quindipan <quindipc@gmail.com>
2023-07-23 14:50:46 -0400

    refactor the personaltiy test onto game window

--- client/src/components/PersonalityTest/PersonalityTest.jsx
+++ client/src/components/PersonalityTest/PersonalityTest.jsx
@@ -1,47 +1,37 @@
+// DEPENDANCIES
 import React, { useState } from "react";
+import personalityTestData from "../../../public/gamedata/personality-test.json";
 
 export default function PersonalityTest({ onComplete }) {
-  // SET POINTS FOR PERSONALITY
-  const [compassionatePoints, setCompassionatePoints] = useState(0);
-  const [pragmaticPoints, setPragmaticPoints] = useState(0);
-  const [curiousPoints, setCuriousPoints] = useState(0);
-  const [ambitiousPoints, setAmbitiousPoints] = useState(0);
-  const [mysteriousPoints, setMysteriousPoints] = useState(0);
-  const [selfReliantPoints, setSelfReliantPoints] = useState(0);
+  // STATE
+  const [personalityData, setPersonalityData] = useState(personalityTestData);
+  const [traitPoints, setTraitPoints] = useState({});
 
+  // SETTING THE POINTS FOR THE TEST
   const handlePersonalityTest = (points, trait) => {
-    switch (trait) {
-      case "Compassionate":
-        setCompassionatePoints((prevPoints) => prevPoints + points);
-        break;
-      case "Pragmatic":
-        setPragmaticPoints((prevPoints) => prevPoints + points);
-        break;
-      case "Curious":
-        setCuriousPoints((prevPoints) => prevPoints + points);
-        break;
-      case "Ambitious":
-        setAmbitiousPoints((prevPoints) => prevPoints + points);
-        break;
-      case "Mysterious":
-        setMysteriousPoints((prevPoints) => prevPoints + points);
-        break;
-      case "Self-reliant":
-        setSelfReliantPoints((prevPoints) => prevPoints + points);
-        break;
-      default:
-        break;
-    }
-    onComplete();
+    setTraitPoints((prevTraitPoints) => ({
+      ...prevTraitPoints,
+      [trait]: (prevTraitPoints[trait] || 0) + points,
+    }));
   };
+
+  // ASSIGN THE TRAIT POINTS FOR THE CHARACTER SELECTION
+  const handleComplete = () => {
+    onComplete(traitPoints);
+  };
+
+  const currentScenario = personalityData[1]; // START WITH THE FIRST SCENARIO
 
   return (
     <div className="personalitytest">
       <h2>Personality Test</h2>
-      <p>Scenario 1: ...</p>
-      <button onClick={() => handlePersonalityTest(1, "Compassionate")}>Choice A</button>
-      <button onClick={() => handlePersonalityTest(1, "Pragmatic")}>Choice B</button>
-      {/* Add more questions and choices as needed */}
+      <p>{currentScenario.text}</p>
+      {currentScenario.choices.map((choice, index) => (
+        <button key={index} onClick={() => handlePersonalityTest(1, choice.trait)}>
+          {choice.text}
+        </button>
+      ))}
+      <button onClick={handleComplete}>Complete</button>
     </div>
   );
 }
