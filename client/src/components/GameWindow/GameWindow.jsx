30fe7477697c5ae32b6b81b714810a3f8009c7cd
Chelsea Quindipan <quindipc@gmail.com>
2023-07-23 09:14:00 -0400

    set up personality test in game window

--- client/src/components/GameWindow/GameWindow.jsx
+++ client/src/components/GameWindow/GameWindow.jsx
@@ -3,87 +3,109 @@ import React, { useState } from "react";
 import "./GameWindow.scss";
 
 // Components
-import PersonalityTest from "../PersonalityTest/PersonalityTest"
-
+import PersonalityTest from "../PersonalityTest/PersonalityTest";
 
 const GameWindow = () => {
   // PROGRESS
-  const [progress, setProgess] = useState(0);
+  const [progress, setProgress] = useState(0);
   const [gameText, setGameText] = useState(
     "Wecome to Mythic Tails: Cursebreaker's Odyssey",
   );
   const [isDarkMode, setIsDarkMode] = useState(false);
-
-  // PERSONALITY TRAITS 
-  const [compassionatePoints, setCompassionatePorts] = useState(0)
-  const [pragmaticPoints, setPragmaticPoints] = useState(0);
 
   // DARK MODE TOGGLE
   const handleDarkMode = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
 
+  //  POINTS FOR PERSONALITY
+  const [compassionatePoints, setCompassionatePoints] = useState(0);
+  const [pragmaticPoints, setPragmaticPoints] = useState(0);
+  const [curiousPoints, setCuriousPoints] = useState(0);
+  const [ambitiousPoints, setAmbitiousPoints] = useState(0);
+  const [mysteriousPoints, setMysteriousPoints] = useState(0);
+  const [selfReliantPoints, setSelfReliantPoints] = useState(0);
 
   // PERSONALITY START GAME TEST
   const handlePersonality = () => {
     if (progress === 0) {
       setGameText(
-        "Welcome to the cursed land of Ethoria, where malevolent forces have plagued the once-thriving realm. Your choices will shape the path ahead, determining whether you'll embark on this journey as the compassionate Guardian Alara or the enigmatic Lost Cursebearer, Nyx."
+        "Welcome to the cursed land of Ethoria, where malevolent forces have plagued the once-thriving realm. Your choices will shape the path ahead, determining whether you'll embark on this journey as the compassionate Guardian Alara or the enigmatic Lost Cursebearer, Nyx.",
       );
-    }
-    else if (progress === 1) {
+      setProgess(1);
+    } else if (progress === 1) {
       setGameText(
         "Scenario 1: The Wounded Creature As you traverse the cursed forest, you encounter a wounded creature crying out in pain.",
       );
-    }
-    else if (progress === 2) {
+      setProgess(2);
+    } else if (progress === 2) {
       setGameText(`Choice A: Approach the creature with empathy, offering aid and healing.
       • Trait: Compassionate
       • Description: You show kindness and concern for others, even in dire circumstances.`);
       setGameText(`Choice B: Remain cautious and proceed with caution, unsure if the creature can be trusted.
       • Trait: Pragmatic
       • Description: You prioritize caution and practicality when faced with uncertain situations.`);
+      setProgess(3)
     } else if (progress === 3) {
       setGameText(`Choice B: Remain cautious and proceed with caution, unsure if the creature can be trusted.
       • Trait: Pragmatic
       • Description: You prioritize caution and practicality when faced with uncertain situations.`);
-    }
-    else if (progress === 4) {
+      setProgess(4)
+      setProgess(4)
+    } else if (progress === 4) {
       setGameText(
         "Scenario 2: The Hidden Path You come across an ancient map that hints at a hidden path leading to a powerful artifact, but it's obscured by a complex puzzle.",
       );
-    }
-    else if (progress === 5) {
+      setProgess(5)
+
+    } else if (progress === 5) {
       setGameText(`Choice A: Take the time to solve the puzzle, intrigued by the prospect of uncovering secrets and hidden knowledge.
       • Trait: Curious
       • Description: You have an insatiable thirst for knowledge and enjoy exploring mysteries.`);
       setGameText(`Choice B: Bypass the puzzle and continue on your journey, valuing progress over curiosity.
       • Trait: Ambitious
       • Description: You are focused on your end goal, willing to make sacrifices to achieve it.`);
+      setProgess(6)
+
+    } else if (progress === 6) {
+      handleNext()
+    } else {
+      // END OF PERSONALITY TEST -- ENTER GAME NOW
     }
-    else if (progress === 6) {
-      setGameText(`Choice B: Bypass the puzzle and continue on your journey, valuing progress over curiosity.
-      • Trait: Ambitious
-      • Description: You are focused on your end goal, willing to make sacrifices to achieve it.`);
-    }
-    else {
-      // END OF PERSONALITY TEST -- ENTER GAME NOW 
-    }
-  }
-  
-  
+  };
+
   // PROGRESS STORY -- AFTER PERSONALITY TEST IS COMPLETED
+  const handleNext = () => {
+    if (progress === 5) {
+      // DETERMINE STARTING CHARACTER
+      let startingCharacter;
+      if (compassionatePoints > pragmaticPoints) {
+        startingCharacter = "Alara, the Guardian";
+      } else if (compassionatePoints < pragmaticPoints) {
+        startingCharacter = "Nyx, the Lost Cursebearer";
+      } else {
+        // PLAYER CHOOSES IF EQUAL PERSONALITY
+        return setProgress(6);
+      }
+      setGameText(
+        `Congratulations! You will begin your adventure as ${startingCharacter} in the cursed land of Ethoria. Your choices will shape the fate of this world and the creatures within. Prepare for a thrilling journey ahead!`,
+      );
 
+      // TODO: Add in the game start
+    } else {
+      // CONTINUE THE STORY
+      setProgress((prevProgress) => prevProgress + 1);
+    }
+  };
 
-  
   // RENDER GAME BASED ON PROGRESS
   if (progress === 6) {
     // PERSONALITY TEST COMPLETED
     return (
-      <div className="personality">
-        <PersonalityTest />
+      <div className="game__personality-test">
+        <PersonalityTest onComplete={handleNext} />
       </div>
-    )
+    );
   } else {
     return (
       <div className={`game ${isDarkMode ? "game__dark" : ""}`}>
@@ -94,29 +116,29 @@ const GameWindow = () => {
             id="switch"
             checked={isDarkMode}
             onChange={handleDarkMode}
-            />
+          />
           <label className="game__switch-label" htmlFor="switch">
             <div className="game__toggle"></div>
           </label>
         </div>
-  
+
         {/* GAME */}
         <h1 className="game__title">{gameText}</h1>
         <div className="game__options">
           {/* INITALIZE START GAME */}
           {progress === 0 && (
-            <button className="game__choice" onClick={handlePersonality}>Play Now!</button>
+            <button className="game__choice" onClick={handlePersonality}>
+              Play Now!
+            </button>
           )}
 
           {/*  PERSONALITY CHOICES */}
           {progress === 6 && (
-            <button className="game__choice" >
-              Choose Your Character
-            </button>
+            <button className="game__choice">Choose Your Character</button>
           )}
           {progress < 6 && (
             <>
-              <button className="game__choice">
+              <button className="game__choice" onClick={handleNext}>
                 Next
               </button>
               <button
@@ -137,7 +159,6 @@ const GameWindow = () => {
       </div>
     );
   }
-  
-}
+};
 
 export default GameWindow;
--- client/src/components/PersonalityTest/PersonalityTest.jsx
+++ client/src/components/PersonalityTest/PersonalityTest.jsx
@@ -1,11 +1,45 @@
 import React, { useState } from "react";
 
-const PersonalityTest = () => {
+const PersonalityTest = ({ onComplete }) => {
+  // SET POINTS FOR PERSONALITY
+  const [compassionatePoints, setCompassionatePoints] = useState(0);
+  const [pragmaticPoints, setPragmaticPoints] = useState(0);
+  const [curiousPoints, setCuriousPoints] = useState(0);
+  const [ambitiousPoints, setAmbitiousPoints] = useState(0);
+  const [mysteriousPoints, setMysteriousPoints] = useState(0);
+  const [selfReliantPoints, setSelfReliantPoints] = useState(0);
 
+  const handlePersonalityTest = (points, trait) => {
+    switch (trait) {
+      case "Compassionate":
+        setCompassionatePoints((prevPoints) => prevPoints + points);
+        break;
+      case "Pragmatic":
+        setPragmaticPoints((prevPoints) => prevPoints + points);
+        break;
+      case "Curious":
+        setCuriousPoints((prevPoints) => prevPoints + points);
+        break;
+      case "Ambitious":
+        setAmbitiousPoints((prevPoints) => prevPoints + points);
+        break;
+      case "Mysterious":
+        setMysteriousPoints((prevPoints) => prevPoints + points);
+        break;
+      case "Self-reliant":
+        setSelfReliantPoints((prevPoints) => prevPoints + points);
+        break;
+      default:
+        break;
+    }
+    onComplete(); 
+  };
 
+  return (
+    <>
+      <div className="personalitytest"></div>
+    </>
+  );
+};
 
-    return (<>
-    </>)
-}
-
-export default PersonalityTest
+export default PersonalityTest;
