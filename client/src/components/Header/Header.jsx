10c2083cb2281df723cd625b4a5984199ad2f385
Chelsea Quindipan <quindipc@gmail.com>
2023-07-24 19:57:19 -0400

    add logo and edit header styles

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,25 +4,30 @@ import { Link } from "react-router-dom";
 import "./Header.scss";
 import HomePage from "../../pages/HomePage/HomePage";
 import GamePage from "../../pages/GamePage/GamePage";
+import colorLogo from "../../assets/images/logo/Mythic-Tails-Logo-Color.svg"
 
 export default function Header() {
   return (
     <>
       <header className="header">
         <nav className="header__nav">
+          <div className="header__logo-container">
+            <img className="header__logo" src={colorLogo} alt="Mythic Tails Logo" />
+          </div>
           <ul className="header__navlist">
-            <Link to="/" element={HomePage}>
+            <Link className="header__navlist-item" to="/" element={HomePage}>
             <li className="header__navlist-item">Home</li>
             </Link>
 
-            <Link to="/play-game" element={GamePage}>
+            <Link className="header__navlist-item" to="/play-game" element={GamePage}>
             <li className="header__navlist-item">Play Now</li>
             </Link>
 
-            {/* <li className="header__navlist-item">Create List</li>
-            <li className="header__navlist-item">Create Details</li>
-            <li className="header__navlist-item">Create Management</li>
+            {/* <li className="header__navlist-item">Creature List</li>
+            <li className="header__navlist-item">Creature Details</li>
+            <li className="header__navlist-item">Creature Management</li>
             <li className="header__navlist-item">Login</li>
+            <li className="header__navlist-item">Sign Up</li>
             <li className="header__navlist-item">Contact</li> */}
           </ul>
         </nav>
