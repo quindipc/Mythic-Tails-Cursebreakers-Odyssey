30b836ee707c477685b9ef83bba480ed7ad7ed7f
Chelsea Quindipan <quindipc@gmail.com>
2023-07-25 14:34:35 -0400

    added endpoint comments

--- server/routes/creatures.js
+++ server/routes/creatures.js
@@ -5,9 +5,12 @@ const router = express.Router();
 // Controller
 const creaturesController = require('../controllers/creaturesController');
 
+// http://localhost:8080/api/creatures
 router.get('/', creaturesController.getAllCreatures) // Get all creatures
     .post('/', creaturesController.addCreature); // Add a creature -- aka player will adopt one for their journey
 
+    
+//http://localhost:8080/api/creatures/3
 router.get('/:id', creaturesController.getCreatureById) // Get specific creature
     .put('/:id', creaturesController.updateCreature) // Update creature information
     .delete('/:id', creaturesController.deleteCreature); // Delete a creature -- aka send creature back to wild
