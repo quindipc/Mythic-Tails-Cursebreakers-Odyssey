dd706661d77613db1c2648cfd276666495451987
Chelsea Quindipan <quindipc@gmail.com>
2023-07-25 20:33:44 -0400

    edit nyx prologue and setup scss

--- client/src/components/Nyx/Nyx.jsx
+++ client/src/components/Nyx/Nyx.jsx
@@ -9,34 +9,34 @@ export default function Nyx() {
         <button className="nyx__next">Next</button>
         {/* Need to display these one at a time -- add logic on the button*/}
         <p className="nyx__prologue">
-          In the realm of Ethoria, where darkness cast a shadow upon the land, a
-          mysterious figure known as the Lost Cursebearer roamed aimlessly.
-          Veiled in obscurity, the Cursebearer was known as Nyx, a name
-          whispered in hushed tones among the cursed inhabitants of the desolate
-          realm. Nyx had long forgotten their true identity, plagued by visions
-          that felt like fragmented memories from another life.
+          In the realm of Ethoria, where darkness casts a shadow upon the land,
+          a mysterious figure known as the Lost Cursebearer roams aimlessly.
+          Veiled in obscurity, the Cursebearer is known as Nyx, a name whispered
+          in hushed tones among the cursed inhabitants of the desolate realm.
+          Nyx has long forgotten their true identity, plagued by visions that
+          feel like fragmented memories from another life.
         </p>
         {/* Next button to continue the story -- add logic on button */}
         <button className="nyx__next">Next</button>
         <p className="nyx__prologue">
           In the heart of Ethoria's cursed lands, where once-thriving forests
-          had withered, and the creatures suffered under the malevolence of the
-          sorceress Aveline's curse, Nyx emerged as an enigmatic being.
-          Unbeknownst to Nyx, they possessed an innate ability to wield dark
+          had withered, and the creatures suffer under the malevolence of the
+          sorceress Aveline's curse, Nyx emerges as an enigmatic being.
+          Unbeknownst to Nyx, they possess an innate ability to wield dark
           magic, tapping into the suffering of the cursed creatures and
           channeling it into formidable power. Guided by cryptic whispers and
-          haunting visions, Nyx found themselves inexorably drawn towards a
+          haunting visions, Nyx finds themselves inexorably drawn towards a
           mysterious artifact known as the "Tear of Radiance."
         </p>
         {/* Next button to continue the story -- add logic on button */}
         <button className="nyx__next">Next</button>
         <p className="nyx__prologue">
-          Rumors and legends whispered that the Tear of Radiance held the power
+          Rumors and legends whisper that the Tear of Radiance holds the power
           to break even the most potent enchantments and lift curses from the
-          land. Nyx believed that obtaining this artifact would hold the key to
-          their true purpose and identity. The visions they experienced showed
-          them an elusive figure, the sorceress Aveline, who was both the source
-          of the curse and a reflection of Nyx's dark abilities.
+          land. Nyx believes that obtaining this artifact will hold the key to
+          their true purpose and identity. The visions they experience show them
+          an elusive figure, the sorceress Aveline, who is both the source of
+          the curse and a reflection of Nyx's dark abilities.
         </p>
         {/* Next button to continue the story -- add logic on button */}
         <button className="nyx__next">Next</button>
@@ -54,12 +54,9 @@ export default function Nyx() {
           trembling voice, "if you have the power, spare me from this wretched
           fate." Nyx encounters a wretched and suffering creature. Its once
           magnificent form is now twisted by a curse, and its eyes plead for
-          mercy. "P-please," the creature pleaded with a trembling voice,
-          "release me from this agony. If you have the power, spare me from this
-          wretched fate."The creature's pained cries tug at Nyx's heart, but
-          they must decide how to proceed.
+          mercy.
         </p>
-  {/* Next button to continue the story -- add logic on button */}
+        {/* Next button to continue the story -- add logic on button */}
         <button className="nyx__next">Next</button>
         {/* Need to map over the scenario, story and choices based on the users choices */}
         <h2 className="nyx__scenario"></h2>
