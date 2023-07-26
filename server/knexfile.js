7e9f92dd8b0f5cb9a3d0066040f6efe269893bb8
Chelsea Quindipan <quindipc@gmail.com>
2023-07-26 11:41:17 -0400

    edit knex file to include new seeds

--- server/knexfile.js
+++ server/knexfile.js
@@ -15,4 +15,35 @@ module.exports = {
   seeds: {
     directory: "./seeds",
   },
+  nyx: {
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.NYX_DB_DATABASE,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds/nyx",
+    },
+  },
+  alara: {
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.ALARA_DB_DATABASE,
+    },
+    migrations: {
+      directory:  "./migrations",
+    },
+    seeds: {
+      directory: "./seeds/alara",
+    },
+  },
 };
+
