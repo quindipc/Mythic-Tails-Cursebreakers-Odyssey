696c5dabc5c0ecf88be491b56bf08eadc640286c
Chelsea Quindipan <quindipc@gmail.com>
2023-07-31 16:44:38 -0400

    edit sizing of game screen and add image to title

--- client/src/components/GameTitle/GameTitle.jsx
+++ client/src/components/GameTitle/GameTitle.jsx
@@ -1,17 +1,20 @@
-// DEPENDANCIES
+// DEPENDENCIES
 import React from "react";
 import "./GameTitle.scss";
 
+// ASSETS
+import MythicTails from "../../assets/images/logo/Mythic-Tails-Logo-Color.svg"
 
 export default function GameTitle({handleStartGame}) {
 
   return (
-    <div className="game__gametitle">
-      <h1 className="game__gametitle--title">
+    <div className="gametitle">
+      <h1 className="gametitle__title">
         Welcome to Mythic Tails: Cursebreaker's Odyssey
       </h1>
+      <img className="gametitle__image" src={MythicTails} alt="Mythic Tails Logo" />
       {/* TODO: CHANGE TO LINK "/CHARACTER-SELECT" */}
-      <button className="game__gametitle-button" onClick={handleStartGame}>
+      <button className="gametitle__button" onClick={handleStartGame}>
         Play Now
       </button>
     </div>
