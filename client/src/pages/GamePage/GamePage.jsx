4bbc129bbaf215d48e965062d71f1553a1467448
Chelsea Quindipan <quindipc@gmail.com>
2023-08-01 16:27:35 -0400

    clean up scss

--- client/src/pages/GamePage/GamePage.jsx
+++ client/src/pages/GamePage/GamePage.jsx
@@ -6,13 +6,7 @@ export default function GamePage() {
 
     return (
         <>
-        
-
             <GameWindow />
-            {/* Login? */}
-            {/* Register? */}
-
-
         </>
     )
 }
