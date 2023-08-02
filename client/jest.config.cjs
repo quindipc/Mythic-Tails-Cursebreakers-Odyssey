9bedd843a61f5433c7e0f7b15343707ee7cd0b63
Chelsea Quindipan <quindipc@gmail.com>
2023-08-02 10:50:00 -0400

    try to fix jest testing? testing not working

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,16 +1,9 @@
-require("@babel/register")({
-  presets: ["@babel/preset-env"],
-});
-
 module.exports = {
-    testEnvironment: "jsdom",
-  
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-      "^.+\\.scss$": "jest-transform-scss",
-    },
-  
-  };
-  
+  testEnvironment: "jsdom",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    "^.+\\.scss$": "jest-transform-scss",
+  },
+  setupFiles: ["@babel/register"],
+};
